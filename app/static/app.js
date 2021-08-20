390a7be03f3e38bc8c3a4a19933000fa230772c7
Murithi Mwitharii <brodiamyzalius@gmail.com>
2021-08-20 16:42:36 +0300

    wave animations

--- app/static/app.js
+++ app/static/app.js
@@ -14,4 +14,12 @@ $(document).ready(function(){
           $('.sidenav').sidenav('close');
       });
 });
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
   
