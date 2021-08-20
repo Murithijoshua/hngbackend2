8c0f60a0defe25578328f941d24073bc089f81a2
Murithi Mwitharii <brodiamyzalius@gmail.com>
2021-08-20 16:18:18 +0300

    side bar fix

--- app/static/app.js
+++ app/static/app.js
@@ -3,8 +3,15 @@ $(document).ready(function(){
   });
   
 
- // collapsable element
-  $(document).ready(function(){
-    $('.collapsible').collapsible();
-  });
-       
+//  // collapsable element
+//   $(document).ready(function(){
+//     $('.collapsible').collapsible();
+//   });
+$(document).ready(function(){
+  $('.sidenav')
+      .sidenav()
+      .on('click tap', 'li a', () => {
+          $('.sidenav').sidenav('close');
+      });
+});
+  
