bcc5881faf3dc40b849f7db5148037521e7783ed
Murithi Mwitharii <brodiamyzalius@gmail.com>
2021-08-20 15:51:00 +0300

    side nav for mobile

--- app/static/app.js
+++ app/static/app.js
@@ -1,18 +1,10 @@
 $(document).ready(function(){
-
-  // Initialize materialize data picker
-  $('select').formSelect();
-
-});
-// for the side navigation
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
-
-  $(document).ready(function(){
     $('.sidenav').sidenav();
   });
   
 
- 
+ // collapsable element
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+       
